cf86a8b6491b59df40f3d283859f06e0f1ccec8d
ViGi <vighnesh7520@gmail.com>
2022-01-28 00:12:50 +0530

    intial commit

---
+++ src/Reverse_number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Reverse_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        System.out.println("Original number- "+num);
+
+        int rev = 0;
+
+        while(num!=0){
+            rev = rev*10 + num%10;      //note- '%' gives last number of the digit
+            num = num/10;               //note- '/' removes last number of the digit
+
+        }
+        System.out.println("Reverse number- "+rev);
+
+    }
+}
