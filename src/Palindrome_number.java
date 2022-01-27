98a96927b447a063e29aad7e906d949309521618
ViGi <vighnesh7520@gmail.com>
2022-01-28 00:45:43 +0530

    initial commit

---
+++ src/Palindrome_number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrome_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:- ");
+
+        int num = sc.nextInt();
+        int org = num;
+        int rev = 0;
+
+        while(num!=0)
+        {
+            rev = rev*10 + num%10;
+            num = num/10;
+        }
+
+        if(org==rev)
+        {
+            System.out.println(org+ " -This is a palindrome number");
+        }
+        else
+        {
+            System.out.println(org+ " -This is not a palindrome number");
+        }
+    }
+}
