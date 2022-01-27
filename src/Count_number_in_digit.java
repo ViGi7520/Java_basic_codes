6eb01ca580686602478893de0f802c30a887bb56
ViGi <vighnesh7520@gmail.com>
2022-01-28 01:21:05 +0530

    Initial commit

---
+++ src/Count_number_in_digit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Count_number_in_digit {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a Number:- ");
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int count = 0;
+
+        while(num>0)
+        {
+            num = num/10;
+            count++;
+        }
+        System.out.println("Number of digits:- " +count);
+    }
+}
