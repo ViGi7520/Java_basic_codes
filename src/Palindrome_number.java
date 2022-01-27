fb91420078ff229321781f6b275597a637e800af
ViGi <vighnesh7520@gmail.com>
2022-01-28 01:07:21 +0530

    improvement

--- src/Palindrome_number.java
+++ src/Palindrome_number.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Palindrome_number {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter a number:- ");
+        Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
         int org = num;
--- src/Reverse_string.java
+++ src/Reverse_string.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Reverse_string {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String");
+        Scanner sc = new Scanner(System.in);
 
         String str = sc.next();
         String rev = "";
