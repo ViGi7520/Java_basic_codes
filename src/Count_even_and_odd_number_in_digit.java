f769f2a7570bc1d00bb1ded24e2c2d8f5c9f1587
ViGi <vighnesh7520@gmail.com>
2022-01-28 01:38:12 +0530

    Initial commit

--- src/Count_even_and_odd_number_in_digit.java
+++ src/Count_even_and_odd_number_in_digit.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
-public class Count_even_and_odd_number_in_digit {
+public class Count_even_and_odd_number_in_digit
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         System.out.println("Enter a Number:- ");
         Scanner sc = new Scanner(System.in);
@@ -14,16 +16,21 @@ public class Count_even_and_odd_number_in_digit {
         while (num > 0) {
             int rem = num % 10;  //note- '%' gives last number of the digit
 
-            if (rem % 2 == 0) {
-                count_even++;
-            } else {
-                count_odd++;
+                if (rem % 2 == 0)
+                 {
+                    count_even++;
+                 }
+                 else
+                 {
+                    count_odd++;
+                 }
+                 num=num/10;          //note- '/' removes last number of the digit
             }
-            num=num/10;          //note- '/' removes last number of the digit
-        }
             System.out.println("Number of Even digits:- " + count_even);
             System.out.println("Number of Odd digits:- " +count_odd);
+
     }
+
 }
 
 
