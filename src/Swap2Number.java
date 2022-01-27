a28d7aa0a8ee807b4e759ea0c0fa46a4ce1a1b6b
ViGi <vighnesh7520@gmail.com>
2022-01-27 14:41:54 +0530

    improvement

---
+++ src/Swap2Number.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Swap2Number {
+    public static void main(String[] args) {
+
+        Scanner num= new Scanner(System.in);
+
+        System.out.println("Enter two numbers...");
+        int a = num.nextInt();
+        int b = num.nextInt();
+
+        System.out.println("before swapping number.. " +a+ " " +b);
+
+        // 1st method
+        /*
+        int c = a;
+        a = b;
+        b = c;
+
+         */
+
+        // 2nd method
+        /*
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+         */
+
+        // 3rd method
+        a = a*b;
+        b = a/b;
+        a = a/b;
+
+        System.out.println("after swapping number.. " +a+ " " +b);
+    }
+}
