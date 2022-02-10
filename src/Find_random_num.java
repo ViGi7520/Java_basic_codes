e3b884ffbf0f1c06506fad08f2c97256f0805835
ViGi <vighnesh7520@gmail.com>
2022-02-10 15:56:34 +0530

    initial commit

---
+++ src/Find_random_num.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Find_random_num {
+    public static void main(String[] args) {
+
+        System.out.println("Find Random num between 0 to ");
+        Scanner sc = new Scanner(System.in);
+        int end = sc.nextInt();
+
+        Random rand=new Random();
+        int rand_int = rand.nextInt(end);
+
+        System.out.println(rand_int);
+    }
+}
