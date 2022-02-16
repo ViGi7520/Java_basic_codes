c56063d2eb73ab9acf1ef03eb3746549e20c547c
ViGi <vighnesh7520@gmail.com>
2022-02-17 03:57:47 +0530

    Initial commit

---
+++ src/Check_duplicate_in_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Check_duplicate_in_array {
+    public static void main(String[] args) {
+
+        boolean status = false;
+
+        String[] a ={"java","C","C++","python","java"};
+
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i].equals(a[j])){
+                    System.out.println("!! Duplicate found !! ** " +a[i]+" **");
+                    status = true;
+                }
+            }
+        }
+            if (!status){
+                System.out.println("No duplicate found");
+            }
+    }
+}
