23da30fbe4bee2d0383caec0da1fd8d4b9b13aec
ViGi <vighnesh7520@gmail.com>
2022-02-17 04:11:59 +0530

    Initial commit

---
+++ src/LinearSearrch.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LinearSearrch {
+    public static void main(String[] args) {
+        int[] a= {10,20,30,40,50};
+
+        System.out.println("Enter a number to search: ");
+        Scanner sc= new Scanner(System.in);
+        int search= sc.nextInt();
+
+        boolean found=false;
+
+        for (int j : a) {
+            if (j == search) {
+                System.out.println("Element Found");
+                found = true;
+            }
+        }
+        if(!found) {
+            System.out.println("Element not Found");
+        }
+    }
+}
